9d4be87b324a775c209f57aa28850ec567203480
MINKIM93 <74599071+MINKIM93@users.noreply.github.com>
2020-12-18 15:06:01 +0900

    Add files via upload

---
+++ SpringMybatisMB2Replay/src/main/java/com/spring/mb2replay/comment/CommentService.java
@@ -0,0 +1,13 @@
+package com.spring.mb2replay.comment;
+
+import java.util.List;
+
+import com.spring.mapper.CommentMapper;
+
+public interface CommentService {
+	public int commentCount() throws Exception;
+	public List<CommentVO> commentListService(int bno) throws Exception;
+	public int commentInsertService(CommentVO comment) throws Exception;
+	public int commentUpdateService(CommentVO comment) throws Exception;
+	public int commentDeleteService(int cno) throws Exception;
+}
